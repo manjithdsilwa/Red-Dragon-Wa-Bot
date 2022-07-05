8b307409f98289b9995e5a08c50ab3baf82cc62b
Induwara Ashinsana <89982429+AshinsanaInduwara@users.noreply.github.com>
2022-07-05 12:35:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `Ｉｍ_Ｉｎｄｕｗａｒａ🔥
+My Creator is Im_Induwara😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94711540155'] //Owner number in aive msg
+global.premium = ['+94711540155'] //Owner Number info
+global.ownernomer = '+94711540155' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎 𝓘𝓶_𝓘𝓷𝓭𝓾𝔀𝓪𝓻𝓪' //Owner Name
+global.botname = ' ☬༒DRAGON࿐' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.btnurl = 'https://www.youtube.com/channel/UCFq4x-jRRGuVouBcm0YWKmA' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = ' ☬༒DRAGON࿐' //Bot Pacage Name
+global.author = '😎 𝓘𝓶_𝓘𝓷𝓭𝓾𝔀𝓪𝓻𝓪❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_Im_kz_T.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://github.com/AshinsanaInduwara' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
